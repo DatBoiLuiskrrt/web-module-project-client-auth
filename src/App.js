b31dc70502a5c19e9d0813d0117b7de387350eb9
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-01-25 20:52:00 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link, Switch, useHistory } from "react-router-dom";
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import AddFriends from "./components/AddFriends";
+import Logout from "./components/Logout";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+  const { push } = useHistory();
+  const isLoggedIn = localStorage.getItem("token");
+  const [friends, setFriends] = useState([]);
+
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <header>
+        <h3>FRIENDS DATABASE</h3>
+        <nav>
+          <Link to="/login">LOGIN</Link>
+          <Link to="/friends-list">FRIENDS LIST</Link>
+          <Link to="/add-friend">ADD FRIEND</Link>
+          <Link to="/logout">LOGOUT</Link>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/logout" component={Logout} />
+        <PrivateRoute
+          exact
+          path="/add-friend"
+          component={AddFriends}
+          friends={friends}
+          setFriends={setFriends}
+        />
+        <PrivateRoute
+          exact
+          path="/friends-list"
+          component={FriendsList}
+          friends={friends}
+          setFriends={setFriends}
+        />
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Login} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+export default function Login() {
+  const { push } = useHistory();
+  const [state, setState] = useState({
+    credentials: {
+      username: "Bloom",
+      password: "Tech",
+    },
+  });
+
+  const handleChange = (e) => {
+    setState({
+      credentials: {
+        ...state.credentials,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:9000/api/login", state.credentials)
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        push("/friends-list");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          name="username"
+          value={state.credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={state.credentials.password}
+          onChange={handleChange}
+        />
+        <button>SUBMIT</button>
+      </form>
+    </div>
+  );
+}
